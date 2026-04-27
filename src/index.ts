e9af8832826f42b88af40ccd689d2225f09f9fdb
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2026-04-27 16:29:22 +0200

    index 2

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+// ─────────────────────────────────────────────────
+// Projet #1 — Hello World
+//
+// Concepts appris :
+//   - process.argv  → lire les arguments du terminal
+//   - console.log   → afficher dans le terminal
+//   - if / else     → logique de base
+// ─────────────────────────────────────────────────
+
+
+// process.argv est un tableau de strings.
+// Voici ce qu'il contient quand tu tapes :
+//   node src/index.ts Harry
+//
+//   process.argv[0] = "/usr/bin/node"   (node lui-même)
+//   process.argv[1] = "/src/index.ts"   (ton fichier)
+//   process.argv[2] = "Harry"           (ton 1er argument)
+//
+// Les arguments UTILISATEUR commencent à l'index 2.
+
+const prenom = process.argv[2]; // peut être undefined si rien n'est tapé
+const langue = process.argv[3]; // peut être undefined aussi
+
